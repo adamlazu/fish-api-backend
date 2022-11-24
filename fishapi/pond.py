17337afe6c9f72038f30b692a02fe3a285140547
Mochamad Adam Lazuardi Agung <90562004+adamlazu@users.noreply.github.com>
2022-11-24 17:40:37 +0700

    get and put api for pond

--- fishapi/pond.py
+++ fishapi/pond.py
@@ -9,28 +9,28 @@ from datetime import datetime
 bp = Blueprint('pond',__name__)
 api = Api(bp)
 
-
-class Ponds(Resource):
-    def get(self):
-        data = get_ponds()
+class Pond(Resource):
+    def get(self, pond_id):
+        ObjInstance = ObjectId(pond_id)
+        filter = {'_id':ObjInstance}
+        data = get_pond(filter)
         return json.loads(dumps(data))
     
-    def post(self):
-        today = datetime.now().strftime("%d/%m/%Y")
-        req = request.json
-        data = {
-            'name':req.get('nama'),
+    def put(self, pond_id):
+        ObjInstance = ObjectId(pond_id)
+        filter = {'_id':ObjInstance}
+        req = request.form
+        newvalues ={"$set":{
+           'name':req.get('nama'),
             'location':req.get('lokasi'),
             'material':req.get('material'),
             'shape':req.get('bentuk'),
             'number':req.get('nomor'),
             'diameter':int(req.get('diameter')),
             'height':int(req.get('tinggi')),
-            'total_fish':int(req.get('jumlah_ikan')),
-            'is_active':False,
-            'created_at': today
-        }
-        insert_pond(data)
-        return {'success':True}
+            'total_fish':int(req.get('jumlah_ikan')), 
+        }}
+        update_pond(filter,newvalues)
+        return {"success":True}
 
-api.add_resource(Ponds, '/API/ponds')
+api.add_resource(Pond, '/API/pond/<pond_id>')
