9ec7af87c1174477685213c9c2d4c096cecfa352
Handrian_W <69663236+HandrianW@users.noreply.github.com>
2022-11-24 17:45:17 +0700

    add CORS

--- fishapi/__init__.py
+++ fishapi/__init__.py
@@ -1,10 +1,13 @@
 import os
 from flask import *
 from . import db
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__,instance_relative_config= True)
     app.config.from_pyfile('settings.cfg',silent=False)
+    CORS(app)
+
 
     try:
         os.makedirs(app.instance_path)
